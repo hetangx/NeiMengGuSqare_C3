17e3037c1346d3d356a5966f2b179b1c11096e5f
hetangx <505888379@qq.com>
2019-07-14 14:56:17 +0800

    已能显示初始7种随机方块

--- sqare/elsHead.c
+++ sqare/elsHead.c
@@ -1,11 +1,112 @@
 #include "elsHead.h"
 
+//背景数组,使用char节省空间
+int g_arrBackGround[20][10] = { 0 };//20行10列，i行j列
+int g_arrSqare[2][4] = { 0 };
+
+
 void OnPaint(HDC hDC)
 {
-	Rectangle(hDC, 0, 0, 300, 600);
+	//创建兼容性DC
+	HDC hMemDC = CreateCompatibleDC(hDC);
+	//创建一张纸
+	HBITMAP hBitmapBack = CreateCompatibleBitmap(hDC, 500, 600);
+	//关联编号和纸
+	SelectObject(hMemDC, hBitmapBack);
+
+	PaintSqare(hMemDC);
+
+
+	//内存DC传递至窗口DC
+	BitBlt(hDC, 0, 0, 300, 600, hMemDC, 0, 0, SRCCOPY);
+
+	//释放
+	DeleteObject(hBitmapBack);
+	DeleteDC(hMemDC);
+}
+
+void PaintSqare(HDC hMemDC)
+{
+	//画大方块
+	Rectangle(hMemDC, 0, 0, 300, 600);
+
+	/*
+	//测试代码，指定某元素为1
+	g_arrBackGround[2][4] = 1;
+	g_arrBackGround[3][3] = 1;
+	g_arrBackGround[3][4] = 1;
+	g_arrBackGround[3][5] = 1;
+	*/
+	
+	int i = 0, j = 0;
+	for (i = 0; i < 20; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (g_arrBackGround[i][j] == 1)
+			{
+				//画小方块
+				Rectangle(hMemDC, j * 30, i * 30, j * 30 + 30, i * 30 + 30);
+			}
+		}
+	}
+}
+
+int CreateRandomSqare()
+{
+	int nIndex = rand()%7;
+	switch (nIndex)
+	{
+	case 0:
+		g_arrSqare[0][0] = 1, g_arrSqare[0][1] = 1, g_arrSqare[0][2] = 0, g_arrSqare[0][3] = 0,		//	|■■  |
+			g_arrSqare[1][0] = 0, g_arrSqare[1][1] = 1, g_arrSqare[1][2] = 1, g_arrSqare[1][3] = 0;	//	| ■■ |
+		break;
+	case 1:
+		g_arrSqare[0][0] = 0, g_arrSqare[0][1] = 1, g_arrSqare[0][2] = 1, g_arrSqare[0][3] = 0,		//	| ■■ |
+			g_arrSqare[1][0] = 1, g_arrSqare[1][1] = 1, g_arrSqare[1][2] = 0, g_arrSqare[1][3] = 0;	//	|■■  |
+		break;
+	case 2:
+		g_arrSqare[0][0] = 0, g_arrSqare[0][1] = 0, g_arrSqare[0][2] = 1, g_arrSqare[0][3] = 0,		//	|  ■ |
+			g_arrSqare[1][0] = 1, g_arrSqare[1][1] = 1, g_arrSqare[1][2] = 1, g_arrSqare[1][3] = 0;	//	|■■■ |
+		break;
+	case 3:
+		g_arrSqare[0][0] = 1, g_arrSqare[0][1] = 0, g_arrSqare[0][2] = 0, g_arrSqare[0][3] = 0,		//	|■   |
+			g_arrSqare[1][0] = 1, g_arrSqare[1][1] = 1, g_arrSqare[1][2] = 1, g_arrSqare[1][3] = 0;	//	|■■■ |
+		break;
+	case 4:
+		g_arrSqare[0][0] = 0, g_arrSqare[0][1] = 1, g_arrSqare[0][2] = 0, g_arrSqare[0][3] = 0,		//	| ■  |
+			g_arrSqare[1][0] = 1, g_arrSqare[1][1] = 1, g_arrSqare[1][2] = 1, g_arrSqare[1][3] = 0;	//	|■■■ |
+		break;
+	case 5:
+		g_arrSqare[0][0] = 1, g_arrSqare[0][1] = 1, g_arrSqare[0][2] = 0, g_arrSqare[0][3] = 0,		//	|■■  |
+			g_arrSqare[1][0] = 1, g_arrSqare[1][1] = 1, g_arrSqare[1][2] = 0, g_arrSqare[1][3] = 0;	//	|■■  |
+		break;
+	case 6:
+		g_arrSqare[0][0] = 1, g_arrSqare[0][1] = 1, g_arrSqare[0][2] = 1, g_arrSqare[0][3] = 1,		//	|■■■■|
+			g_arrSqare[1][0] = 0, g_arrSqare[1][1] = 0, g_arrSqare[1][2] = 0, g_arrSqare[1][3] = 0;	//	|    |
+		break;
+
+	default:
+		break;
+	}
+	return nIndex;
+}
+
+void CopySqareToBack()
+{
+	int i = 0, j = 0;
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			g_arrBackGround[i][j + 3] = g_arrSqare[i][j];
+		}
+	}
 }
 
 void OnCreate()
 {
-
+	srand((unsigned int)time(NULL));
+	CreateRandomSqare();
+	CopySqareToBack();
 }
--- sqare/elsHead.h
+++ sqare/elsHead.h
@@ -1,8 +1,17 @@
 #ifndef DEF_ELS_HEAD
 #define DEF_ELS_DEAD
+
 #include <Windows.h>
+#include <time.h>
 
 void OnPaint(HDC hDC);//处理消息
+
+void PaintSqare(HDC hMemDC);//显示方块
+
+int CreateRandomSqare();//创建随机方块
+
+void CopySqareToBack();//随机方块贴进背景
+
 void OnCreate();
 
 #endif // !DEF_ELS_HEAD
